79b9729c531dbc0df978f4b97b4ad80c846c6055
Dicko Finus <dickofn@gmail.com>
2019-06-10 19:55:02 +0700

    lint

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
-import VueScrollReveal from 'vue-scroll-reveal';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
+import VueScrollReveal from "vue-scroll-reveal";
 
 Vue.use(VueScrollReveal);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
