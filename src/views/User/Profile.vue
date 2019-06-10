67c50077068d060c7d00301d9e2a582f3ea1ec0f
Dicko Finus <dickofn@gmail.com>
2019-06-10 20:31:35 +0700

    added card

--- src/views/User/Profile.vue
+++ src/views/User/Profile.vue
@@ -58,7 +58,19 @@
             <div class="schedule-partner-title--text">Partner :</div>
           </div>
           <div class="schedule-partner-content">
-            <div class="schedule-partner-content--calendar"></div>
+            <div class="schedule-partner-content--partner">
+              <card
+                class="partner-card"
+                v-for="talent in talents"
+                :key="talent.id"
+                :thumbnail="talent.thumbnail"
+                :rating="talent.rating"
+                :name="talent.name"
+                :talent="talent.talent"
+                :id="talent.id"
+                :type="'small'"
+              ></card>
+            </div>
           </div>
         </div>
       </div>
@@ -106,13 +118,14 @@
  */
 import myHeader from "@/components/Shared/Header";
 import myFooter from "@/components/Shared/Footer";
+import card from "@/components/Catalog/Card";
 import Bio from "@/components/User/Profile/Bio";
 import Music from "@/components/User/Profile/Music";
 import Video from "@/components/User/Profile/Video";
 import Review from "@/components/User/Profile/Review";
 
 export default {
-  data() {
+  data () {
     return {
       coverPic: {
         background: "url(" + require("@/assets/p_bg.png") + ")",
@@ -121,17 +134,76 @@ export default {
       },
       profilePic: require("@/assets/p_ph.jpg"),
       genres: ["jazz", "pop", "edm", "classic"],
-      currentTab: "Bio"
+      currentTab: "Bio",
+      talents: [
+        {
+          thumbnail: require("@/assets/singer-1.jpg"),
+          id: "1",
+          rating: "5",
+          name: "Talent1",
+          talent: "Singer"
+        },
+        {
+          thumbnail: require("@/assets/singer-1.jpg"),
+          id: "2",
+          rating: "4",
+          name: "Talent2",
+          talent: "Musician"
+        },
+        {
+          thumbnail: require("@/assets/singer-1.jpg"),
+          id: "3",
+          rating: "5",
+          name: "Talent3",
+          talent: "Dancer"
+        },
+        {
+          thumbnail: require("@/assets/singer-1.jpg"),
+          id: "4",
+          rating: "3",
+          name: "Talent4",
+          talent: "MC"
+        },
+        {
+          thumbnail: require("@/assets/singer-1.jpg"),
+          id: "5",
+          rating: "4",
+          name: "Talent5",
+          talent: "DJ"
+        },
+        {
+          thumbnail: require("@/assets/singer-1.jpg"),
+          id: "6",
+          rating: "2",
+          name: "Talent6",
+          talent: "Band"
+        },
+        {
+          thumbnail: require("@/assets/singer-1.jpg"),
+          id: "7",
+          rating: "5",
+          name: "Talent7",
+          talent: "Musician"
+        },
+        {
+          thumbnail: require("@/assets/singer-1.jpg"),
+          id: "8",
+          rating: "0",
+          name: "Talent8",
+          talent: "Singer"
+        }
+      ]
     };
   },
   methods: {
-    menuChanged(payload) {
+    menuChanged (payload) {
       this.currentTab = payload;
     }
   },
   components: {
     myHeader,
     myFooter,
+    card,
     Bio,
     Music,
     Video,
@@ -325,6 +397,27 @@ export default {
           background-color: $color-grey-dark-1;
           height: 50rem; // TODO: fixed height, content coming soon
         }
+
+        &--partner {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: space-between;
+
+          height: 50rem;
+          overflow-y: scroll;
+
+          .partner-card {
+            width: 25%;
+
+            @media only screen and (max-width: $bp-lg) {
+              width: 33%;
+            }
+
+            @media only screen and (max-width: $bp-sm) {
+              width: 50%;
+            }
+          }
+        }
       }
     }
   }
@@ -335,8 +428,8 @@ export default {
   padding: 4rem 10vw;
 
   @media only screen and (max-width: $bp-sm) {
-      padding: 4rem 2vw;
-    }
+    padding: 4rem 2vw;
+  }
 
   &__menu {
     margin-bottom: 4rem;
